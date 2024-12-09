93835cb23a41b7a4b274ccc53eebeb38bbff1b34
mingyuchoo <mingyuchoo@gmail.com>
2024-12-10 08:29:22 +0900

    updated

--- comparison/ADTs/adts.hs
+++ comparison/ADTs/adts.hs
@@ -1,5 +1,7 @@
 #!/usr/bin/env runghc
 
+{-# LANGUAGE GADTs  #-}
+
 -- -----------------------------------------
 -- Sum Type(Union type) vs. Product Type
 -- - Sum Type: unions, variants, ...
@@ -8,7 +10,7 @@
 
 --
 -- Sum Type
--- 
+-- e.g.: Either, Maybe, ...
 data Result = Success String
             | Failure String
             deriving Show 
@@ -19,14 +21,54 @@ process n
   | otherwise = Failure "Non-positive number"
 
 --
+-- Sum Type
+-- - GADT (Gneralized Algebraic Data Types)
+-- 
+data Expr a where
+  IntLit  :: Int -> Expr Int
+  BoolLit :: Bool -> Expr Bool
+  Add     :: Expr Int -> Expr Int -> Expr Int
+
+eval :: Expr a -> a
+eval (IntLit n) = n
+eval (BoolLit b) = b
+eval (Add x y ) = eval x + eval y
+
+--
 -- Product Type
+-- - tuple type
 -- 
 type Point = (Int, Int)
 
 move :: Point -> Point
 move (x, y) = (x + 1, y + 1)
 
+--
+-- Product type
+-- - record type
+--
+data Coordinate = Coordinate { x :: Float
+                             , y :: Float
+                             }
+
+distance :: Coordinate -> Coordinate -> Float
+distance (Coordinate x1 y1) (Coordinate x2 y2) =
+  sqrt ((x2 - x1)^2 + (y2 - y1)^2)
+  
+
+--
 -- main
+--
 main = do
   print $ process 10
+
+  let expr1 = IntLit 4
+  let expr2 = IntLit 6
+  let sumExpr = Add expr1 expr2
+  print $ eval sumExpr
+  
   print $ move (2, 3)
+
+  let pointA = Coordinate 3.0 4.0
+  let pointB = Coordinate 0.0 0.0
+  print $ distance pointA pointB
--- comparison/ADTs/adts.ml
+++ comparison/ADTs/adts.ml
@@ -6,7 +6,9 @@
  * - Product Type: tuples, records, ...
  *)
 
-(* Sume Type *)
+(* Sume Type
+ * - union
+ *)
 type result = Success of string | Failure of string
 
 let process n =
@@ -14,18 +16,41 @@ let process n =
   | 0 -> Success "Positive number"
   | _ -> Failure "Non-positive number"
 
-(* Product Type *)
-type point = int * int
-
-let move (x, y) = (x + 1, y + 1)
-
-(* main *)
 let () =
   match process 10 with
   | Success msg -> print_endline msg
   | Failure msg -> print_endline msg
 
+(* Product Type
+ * - tuple
+ *)
+type point = int * int
+
+let move (x, y) = (x + 1, y + 1)
+
 let () =
   (2, 3)
   |> move
   |> fun (x, y) -> Printf.printf "New position: (%d, %d)\n" x y
+
+(* Product Type
+ * - record
+ *)
+type coordinate = { x: float; y: float }
+
+let distance p1 p2 =
+  let dx = p2.x -. p1.x in
+  let dy = p2.y -. p1.x in
+  sqrt ((dx *. dx) +. (dy *. dy))
+
+let point1 = { x= 3.0; y= 4.0 }
+
+let point2 = { x= 0.0; y= 0.0 }
+
+let () =
+  let dist = distance point1 point2 in
+  match dist with
+  | d when d >= 0.0 ->
+      Printf.printf "The distance between (%f, %f) and (%f, %f) is %f\n"
+        point1.x point1.y point2.x point2.y d
+  | _ -> Printf.printf "Distance cannot be negative.\n"
